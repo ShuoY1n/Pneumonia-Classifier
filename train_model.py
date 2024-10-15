861987343994b1f2f60af3d60e412024cfeaedb7
ShuoY1n <jackshuoyi@gmail.com>
2024-10-15 15:51:17 -0400

    worked on basic training code

--- train_model.py
+++ train_model.py
@@ -6,3 +6,20 @@ import torch.optim as optim
 from torch.utils.data import DataLoader, Dataset
 from torchvision import transforms, models
 from sklearn.metrics import accuracy_score
+
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+
+class XrayDataset(Dataset):
+
+    def __init__(self, root_dir, transform=None):
+        self.root_dir = root_dir
+        self.transform = transform
+        self.image_paths = []
+        self.labels = []
+
+        for lable in ["NORMAL", "PNEUMONIA"]:
+            class_dir = os.path.join(root_dir, lable)
+            for img_name in os.listdir(class_dir):
+                self.image_paths.append(os.path.join(class_dir, img_name))
+                self.labels.append(0x if lable == "NORMAL" else 1)
+
